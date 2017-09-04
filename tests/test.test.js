c5d2b6fdd649850b73d1772dbd335c745c97ada2
Szikszai Gusztáv <guszti5@hotmail.com>
2017-09-04 14:46:41 +0200

    Update 2017-09-04T12:46:39.251Z

--- tests/test.test.js
+++
@@ -1,7 +0,0 @@
-const component = require('../source/Main.elm')
-
-test('Renders simple component', () => {
-  component.Elm.Main.fullscreen()
-  document.querySelector('button').click()
-  expect(document.body.textContent).toContain('-1')
-})
